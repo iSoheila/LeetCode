a0a233cf4364f572eabfce8f009e634975746a73
Soheila <15472333+iSoheila@users.noreply.github.com>
2023-08-27 11:44:53 -0700

    removed the main()

--- 1657_two_strings_r_close.py
+++ 1657_two_strings_r_close.py
@@ -2,30 +2,6 @@ from collections import Counter
 # 1657. Determine if Two Strings Are Close
 # https://leetcode.com/problems/determine-if-two-strings-are-close/description/
 
-def main():
-    
-    '''
-    Input: word1 = "abc", word2 = "bca"
-    Output: true
-    '''
-    print(close_strings("abc", "bca"))
-    assert(close_strings("abc", "bca"))
-    '''
-    Input: word1 = "a", word2 = "aa"
-    Output: false
-    '''
-    print(close_strings("a", "aa"))
-    assert(close_strings("a", "aa")==False)
-
-    '''
-    Input: word1 = "cabbba", word2 = "abbccc"
-    Output: true
-    '''
-    print(close_strings("cabbba", "abbccc"))
-    assert(close_strings("cabbba", "abbccc"))
-
-   
-
 def close_strings(word1, word2):
      
     if len(word1)!= len(word2) or set(word1) != set(word2):
@@ -37,4 +13,22 @@ def close_strings(word1, word2):
 
     return Counter(c1.values()) == Counter(c2.values())
 
-main()
+
+
+'''
+Input: word1 = "abc", word2 = "bca"
+Output: true
+'''
+assert(close_strings("abc", "bca"))
+'''
+Input: word1 = "a", word2 = "aa"
+Output: false
+'''
+assert(close_strings("a", "aa")==False)
+'''
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+'''
+assert(close_strings("cabbba", "abbccc"))
+
+
